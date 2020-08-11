9f9276f91e0c929ee91f0a42dd5f766d7a1d40e7
Joshua <48163249+JoshuaP199@users.noreply.github.com>
2020-08-11 15:23:56 -0400

    start

---
+++ gui_tut.py
@@ -0,0 +1,55 @@
+#Created by Joshua Perez
+
+import tkinter
+from tkinter import *
+from PIL import ImageTk, Image
+
+#can make def's of each game and write all the code here in each one or find a way
+#to link another file and run it through the def.
+
+def interface():
+    #Window Name
+    window = tkinter.Tk()
+    window.title('Games')
+    window.geometry("500x700")
+
+    #Image/logo
+    path = "square.jpg" #need a png and need to figure out resizing
+    img = ImageTk.PhotoImage(Image.open(path))
+    panel = tkinter.Label(window, image = img,)
+    panel.pack()
+
+    #Beginning Message
+    var = StringVar()
+    label = Label(window, textvariable = var, pady = 25)
+    var.set("Choose a game to play!")
+    label.pack()
+
+    #Games
+    def rps():
+        import RockPaperScissors
+        RockPaperScissors.game()    
+
+    #Game Buttons
+    GMC = tkinter.Button(text="Guess My Card", bd = 3, width = 20, state = DISABLED)
+    HM = tkinter.Button(text ="Hangman", bd = 3, width = 20, state = DISABLED)
+    GMN = tkinter.Button(text="Guess My Number", bd = 3, width = 20, state = DISABLED)
+    RPS = tkinter.Button(text="Rock Paper Scissors", command = rps, bd = 3, width = 20)
+    BS = tkinter.Button(text="BattleShip", bd = 3, width = 20, state = DISABLED)
+    TTT = tkinter.Button(text="Tic Tac Toe", bd = 3, width = 20, state = DISABLED)
+
+    GMC.pack()
+    HM.pack()
+    GMN.pack()
+    RPS.pack()
+    BS.pack()
+    TTT.pack()
+    
+    #attempt at trying to close window
+    def close_window():
+        window.destroy()
+
+    window.mainloop() #Always at bottom of window code
+
+interface()
+#figure out a way to close the menu window when the user starts a game
---
+++ update.bat
@@ -0,0 +1,6 @@
+@echo off
+
+cd C:\Users\joshp\OneDrive\Desktop\test
+pyinstaller --onefile gui_tut.py
+
+pause
