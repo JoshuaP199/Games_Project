afcf98dc29fa6322aa29d4968100c15573e5a012
Joshua <48163249+JoshuaP199@users.noreply.github.com>
2020-08-15 23:32:38 -0400

    comments

--- update.bat
+++ update.bat
@@ -1,5 +1,7 @@
 @echo off
+::Delete all gui.tut files EXCEPT for the py file and delete the build and cache folders
 
+::change path to where it is on your pc
 cd C:\Users\joshp\OneDrive\Desktop\test
 pyinstaller --onefile gui_tut.py
 
